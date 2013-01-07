ca765144771090d4ec00523a464dc5371f5e07b3
delta <delta4d@gmail.com>
2013-01-07 21:41:32 +0800

    lalala

---
+++ bar-config.h
@@ -0,0 +1,29 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  16
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 3
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+//#define BAR_FONT       "-*-terminus-medium-r-normal-*-12-*-*-*-*-*-*-*","fixed"
+#define BAR_FONT       "-*-stlarch-medium-r-*-*-10-*-*-*-*-*-*-*","-*-ohsnap-medium-r-normal-*-12-*-*-*-*-*-*-*"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 4
+/* Color palette */
+#define COLOR0  0x111111 /* background */
+#define COLOR1  0xc4c4c4 /* foreground */
+#define COLOR2  0x283B5D
+#define COLOR3  0x852a52
+#define COLOR4  0xadbd00
+#define COLOR5  0xec9d08
+#define COLOR6  0x6b0f38
+#define COLOR7  0x212122
+#define COLOR8  0x333333
+#define COLOR9  0xdedede
+
